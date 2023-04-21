e16b40c12e76de052f006ea2bc67e0b0b62be32d
Felix Zumstein <fzumstein@gmail.com>
2023-04-21 10:34:20 +0200

    ssl + static files

--- app/server_django.py
+++ app/server_django.py
@@ -1,24 +1,32 @@
 # Based on https://arunrocks.com/django-application-in-one-file/
 # Run with: python app/server_django.py runserver
+# pip install django-sslserver
+# python app/server_django.py runsslserver --certificate certs/localhost+2.pem --key certs/localhost+2-key.pem
+from pathlib import Path
 import sys
 import json
 
 from django.conf import settings
 from django.urls import path
 from django.http import JsonResponse
+from django.conf.urls.static import static
 
 import xlwings as xw
 
+BASE_DIR = Path(__file__).resolve().parent
+
 settings.configure(
-	DEBUG=True,
-	SECRET_KEY="changeme",
-	ROOT_URLCONF=__name__,
+    DEBUG=True,
+    SECRET_KEY="changeme",
+    ROOT_URLCONF=__name__,
+    INSTALLED_APPS=["sslserver"],
+    STATICFILES_DIRS=[BASE_DIR],
 )
 
 
 def hello(request):
     # Instantiate a book object with the parsed request body
-    data = json.loads(request.body.decode('utf-8'))
+    data = json.loads(request.body.decode("utf-8"))
     book = xw.Book(json=data)
 
     # Use xlwings as usual
@@ -34,10 +42,11 @@ def hello(request):
 
 
 urlpatterns = [
-	path("hello", hello),
-]
+    path("", lambda request: JsonResponse({"status": "ok"})),
+    path("hello", hello),
+]  + static('/', document_root=BASE_DIR)
 
 if __name__ == "__main__":
-	from django.core.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
 
-	execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
